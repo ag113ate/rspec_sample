126aefc7974ab33ccccc938cb784cd9a2b14ef69
ag113ate <shota.terui@beach.ocn.ne.jp>
2018-12-28 12:18:56 +0000

    Add spec sample

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "姓、名を登録すると、姓名が取得できること" do
+    user = User.new(last_name:  "侍",
+                  first_name: "太郎")
+
+    expect(user.full_name).to eq "侍 太郎"
+  end
 end
