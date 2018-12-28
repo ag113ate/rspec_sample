72399b374d59926e536315a8689595c32b49f8da
ag113ate <shota.terui@beach.ocn.ne.jp>
2018-12-28 14:19:53 +0000

    Add Homes action spec

--- spec/controllers/homes_controller_spec.rb
+++ spec/controllers/homes_controller_spec.rb
@@ -3,9 +3,15 @@ require 'rails_helper'
 RSpec.describe HomesController, type: :controller do
 
   describe "GET #index" do
-    it "returns http success" do
+    before do
       get :index
+    end
+    it "returns http success" do
       expect(response).to have_http_status(:success)
+    end
+
+    it 'リクエストは200 OKとなること' do
+      expect(response.status).to eq 200
     end
   end
 
